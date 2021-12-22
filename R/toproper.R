72c6889c4ddc0e61ac39abe45acb546bf712dc3a
Matthijs S. Berends <m.s.berends@umcg.nl>
2021-12-22 15:06:43 +0100

    toproper()

---
+++ R/toproper.R
@@ -0,0 +1,36 @@
+# ===================================================================== #
+#  An R package by Certe:                                               #
+#  https://github.com/certe-medical-epidemiology                        #
+#                                                                       #
+#  Licensed as GPL-v2.0.                                                #
+#                                                                       #
+#  Developed at non-profit organisation Certe Medical Diagnostics &     #
+#  Advice, department of Medical Epidemiology.                          #
+#                                                                       #
+#  This R package is free software; you can freely use and distribute   #
+#  it for both personal and commercial purposes under the terms of the  #
+#  GNU General Public License version 2.0 (GNU GPL-2), as published by  #
+#  the Free Software Foundation.                                        #
+#                                                                       #
+#  We created this package for both routine data analysis and academic  #
+#  research and it was publicly released in the hope that it will be    #
+#  useful, but it comes WITHOUT ANY WARRANTY OR LIABILITY.              #
+# ===================================================================== #
+
+#' Proper Case for Text
+#' 
+#' @param text text to transform
+#' @param every_word set captial to every word
+#' @export
+toproper <- function(text,
+                     every_word = FALSE,
+                     ...) {
+  text <- as.character(text)
+  titlecase <- tools::toTitleCase(text)
+  if (isTRUE(every_word)) {
+    titlecase
+  } else {
+    substr(text, 1, 1) <- substr(titlecase, 1, 1)
+    text
+  }
+}
